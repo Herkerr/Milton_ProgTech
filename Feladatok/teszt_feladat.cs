4ae547a200be6d026c44fbfb9d5853a10ad57f33
djbukfenc <02tesok@gmail.com>
2024-03-24 19:58:48 +0100

    teszt_feladat_megoldas.cs
    T42YXJ

--- Feladatok/teszt_feladat.cs
+++ Feladatok/teszt_feladat.cs
@@ -5,3 +5,7 @@ class HaziMacska:Macska{}
 
 
 // 1. Feladat: Vigyünk fel egy legalább 4 osztályból álló öröklődési láncot az Ember-ra!
+class Allat{}
+class Gerinces:Allat{}
+class Emlos:Gerinces{}
+class Ember:Emlos{}
